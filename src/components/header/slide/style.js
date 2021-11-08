f56749fae03c7fdb7c5a275f42f7afb0fc30620f
Alan Fernandes <81441721+Alanf3878@users.noreply.github.com>
2021-11-08 13:46:26 -0300

    ajustes

--- src/components/header/slide/style.js
+++ src/components/header/slide/style.js
@@ -110,7 +110,6 @@ justify-content: center;
 align-content: space-around;
 height: 33%;
 `
-<<<<<<< HEAD
 export const SampleNextArrow = styled.div`
 top:300px;
 left: 0;
@@ -121,24 +120,7 @@ z-index: 99;
     top: calc(50% - 10px);
     position: absolute;
     font-size: 80px;
-
 }
-=======
-export const Btnseta = styled.button`
-top: 250px;
-left: 0;
-width: 70px;
-height: 70px;
-border-radius: 50%;
-font-size: 25px;
-border:none;
-background-color:#f8ca11;
-&:hover {
-        background-color:#f8ca11;
-        cursor: pointer;
-        color: #fff;
-    }
->>>>>>> 220e53f137f0924283a504304eb0f98b47c86061
 `
 export const SamplePrevArrow = styled(SampleNextArrow)`
 top:400px;
